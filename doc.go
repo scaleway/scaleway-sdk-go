87f9b6d425868574211a5798f064f06e8c8486bb
MathiasB <mbeke@online.net>
2019-04-29 11:32:29 +0200

    updated examples

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package scalewaysdkgo is the Scaleway APi SDK for Go.
+// Package scalewaysdkgo is the Scaleway API SDK for Go.
 //
 // In order to use the available APIs, create a `Client`. Once created, it can be used to instantiate an API.
 // To use the `instance` API, for example, instantiate it (with the client object) `instance.NewApi(client)`.
