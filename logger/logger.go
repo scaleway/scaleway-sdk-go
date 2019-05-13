b39c13fdbf0279defaae1c42a3f8ce0b573060d8
Olivier Cano <kindermoumoute@users.noreply.github.com>
2019-05-13 16:35:34 +0200

    feat: add logger (#17)
    * feat: add logger
    
    * Simpler tests

---
+++ logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+type LogLevel int
+
+const (
+	// LogLevelDebug indicates Debug severity.
+	LogLevelDebug LogLevel = iota
+	// LogLevelInfo indicates Info severity.
+	LogLevelInfo
+	// LogLevelWarning indicates Warning severity.
+	LogLevelWarning
+	// LogLevelError indicates Error severity.
+	LogLevelError
+)
+
+// severityName contains the string representation of each severity.
+var severityName = []string{
+	LogLevelDebug:   "DEBUG",
+	LogLevelInfo:    "INFO",
+	LogLevelWarning: "WARNING",
+	LogLevelError:   "ERROR",
+}
+
+// Logger does underlying logging work for scaleway-sdk-go.
+type Logger interface {
+	// Debugf logs to DEBUG log. Arguments are handled in the manner of fmt.Printf.
+	Debugf(format string, args ...interface{})
+	// Infof logs to INFO log. Arguments are handled in the manner of fmt.Printf.
+	Infof(format string, args ...interface{})
+	// Warningf logs to WARNING log. Arguments are handled in the manner of fmt.Printf.
+	Warningf(format string, args ...interface{})
+	// Errorf logs to ERROR log. Arguments are handled in the manner of fmt.Printf.
+	Errorf(format string, args ...interface{})
+}
+
+// SetLogger sets logger that is used in by the SDK.
+// Not mutex-protected, should be called before any scaleway-sdk-go functions.
+func SetLogger(l Logger) {
+	logger = l
+}
---
+++ logger/logger_test.go
@@ -0,0 +1,70 @@
+package logger
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/scaleway/scaleway-sdk-go/internal/testhelpers"
+)
+
+var (
+	expectedErrorf   = "ERROR: cd"
+	expectedWarningf = "WARNING: ij"
+	expectedInfof    = "INFO: op"
+	expectedDebugf   = "DEBUG: uv"
+)
+
+func TestDebug(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logThings(newLogger(buf, LogLevelDebug))
+	testThings(t, []string{
+		expectedErrorf,
+		expectedWarningf,
+		expectedInfof,
+		expectedDebugf,
+	}, buf.String())
+}
+
+func TestInfo(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logThings(newLogger(buf, LogLevelInfo))
+	testThings(t, []string{
+		expectedErrorf,
+		expectedWarningf,
+		expectedInfof,
+	}, buf.String())
+}
+
+func TestWarning(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logThings(newLogger(buf, LogLevelWarning))
+	testThings(t, []string{
+		expectedErrorf,
+		expectedWarningf,
+	}, buf.String())
+}
+
+func TestError(t *testing.T) {
+	buf := &bytes.Buffer{}
+	logThings(newLogger(buf, LogLevelError))
+	testThings(t, []string{
+		expectedErrorf,
+	}, buf.String())
+}
+
+func testThings(t *testing.T, expectedEvents []string, actualOutput string) {
+	lines := strings.Split(actualOutput, "\n")
+	for i, line := range lines[:len(lines)-1] { // last line is always empty
+		tmp := strings.Split(line, " ")
+		actualMessage := strings.Join(append([]string{tmp[0]}, tmp[3:]...), " ") // date and hour is not kept
+		testhelpers.Equals(t, expectedEvents[i], actualMessage)
+	}
+}
+
+func logThings(log Logger) {
+	log.Errorf("c%s", "d")
+	log.Warningf("i%s", "j")
+	log.Infof("o%s", "p")
+	log.Debugf("u%s", "v")
+}
