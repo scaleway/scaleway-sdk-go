02c1683fdfe8a0d864427e638bc99623ce926a46
Quentin Brosse <quentin.brosse@icloud.com>
2019-05-24 16:10:07 +0200

    feat: implement async internal package

---
+++ internal/async/wait.go
@@ -0,0 +1,76 @@
+package async
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	defaultInterval = time.Second
+	defaultTimeout  = time.Minute * 5
+)
+
+// WaitOptions defines the waiting options.
+type WaitSyncConfig struct {
+	get      func() (value interface{}, err error, stopCondition bool)
+	interval func() <-chan time.Time
+	timeout  time.Duration
+}
+
+// WaitSync waits and returns when a given stop condition is true or if an error occurs.
+//
+// In this sync implementation, the timeout could be longer than expected.
+func WaitSync(config *WaitSyncConfig) (terminalValue interface{}, err error) {
+	// initialize configuration
+	if config.interval == nil {
+		fmt.Println("config.interval is nil")
+		config.interval = func() <-chan time.Time {
+			return time.After(defaultInterval)
+		}
+	}
+	if config.timeout == 0 {
+		config.timeout = defaultTimeout
+	}
+
+	type payload struct {
+		value interface{}
+		err   error
+	}
+
+	result := make(chan payload, 1)
+	timeout := make(chan bool, 1)
+
+	go func() {
+		for {
+			// get the payload
+			value, err, stopCondition := config.get()
+			p := payload{value, err}
+
+			// send the payload
+			if err != nil || stopCondition {
+				result <- p
+				return
+			}
+
+			// waiting for an interval before next get() call or a timeout
+			select {
+			case <-timeout:
+				return
+			case <-config.interval():
+				// sleep
+			}
+		}
+	}()
+
+	// waiting for a result or a timeout
+	select {
+	case res := <-result:
+		if res.err != nil {
+			return nil, err
+		}
+		return res.value, nil
+	case <-time.After(config.timeout):
+		timeout <- true
+		return nil, fmt.Errorf("timeout after %v", config.timeout)
+	}
+}
---
+++ internal/async/wait_test.go
@@ -0,0 +1,142 @@
+package async
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/scaleway/scaleway-sdk-go/internal/testhelpers"
+)
+
+const flakiness = time.Millisecond * 500
+
+type value struct {
+	doneIterations int
+	totalDuration  time.Duration
+}
+
+func getMock(iterations int, sleepTime time.Duration) func() (interface{}, error, bool) {
+	cpt := iterations
+	var startTime time.Time
+
+	return func() (interface{}, error, bool) {
+		if cpt == iterations {
+			startTime = time.Now()
+		}
+		cpt -= 1
+
+		// fake working time
+		time.Sleep(sleepTime)
+
+		v := &value{
+			doneIterations: iterations - cpt,
+			totalDuration:  time.Since(startTime),
+		}
+		return v, nil, cpt == 0
+	}
+}
+
+func getFibonacciInterval() func() <-chan time.Time {
+	x, y := 0, 1
+	return func() <-chan time.Time {
+		x, y = y, x+y
+		fmt.Println("fib>", time.Duration(x)*time.Second)
+		return time.After(time.Duration(x) * time.Second)
+	}
+}
+
+func TestWaitSync(t *testing.T) {
+	testsCases := []struct {
+		name     string
+		config   *WaitSyncConfig
+		expValue interface{}
+		expErr   error
+	}{
+		{
+			name: "With default timeout and interval",
+			config: &WaitSyncConfig{
+				get: getMock(2, 0),
+			},
+			expValue: &value{
+				doneIterations: 2,
+				totalDuration:  time.Second,
+			},
+		},
+		{
+			name: "With useless timeout",
+			config: &WaitSyncConfig{
+				get:     getMock(2, time.Second),
+				timeout: 4 * time.Second,
+			},
+			expValue: &value{
+				doneIterations: 2,
+				totalDuration:  3 * time.Second,
+			},
+		},
+		{
+			name: "Should timeout",
+			config: &WaitSyncConfig{
+				get:     getMock(2, 2*time.Second),
+				timeout: time.Second,
+			},
+			expValue: nil,
+			expErr:   fmt.Errorf("timeout after 1s"),
+		},
+		{
+			name: "With interval",
+			config: &WaitSyncConfig{
+				get: getMock(2, 0),
+				interval: func() <-chan time.Time {
+					return time.After(time.Second * 2)
+				},
+			},
+			expValue: &value{
+				doneIterations: 2,
+				totalDuration:  2 * time.Second,
+			},
+		},
+		{
+			name: "With fibonacci interval",
+			config: &WaitSyncConfig{
+				get:      getMock(5, 0),
+				interval: getFibonacciInterval(),
+			},
+			expValue: &value{
+				doneIterations: 5,
+				totalDuration:  7 * time.Second,
+			},
+		},
+		{
+			name: "Should timeout with interval",
+			config: &WaitSyncConfig{
+				get:     getMock(2, time.Second),
+				timeout: 2 * time.Second,
+				interval: func() <-chan time.Time {
+					return time.After(time.Second * 2)
+				},
+			},
+			expValue: nil,
+			expErr:   fmt.Errorf("timeout after 2s"),
+		},
+	}
+	for _, c := range testsCases {
+		c := c // do not remove me
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			terminalValue, err := WaitSync(c.config)
+
+			testhelpers.Equals(t, c.expErr, err)
+
+			if c.expValue != nil {
+				exp := c.expValue.(*value)
+				acc := terminalValue.(*value)
+				testhelpers.Equals(t, exp.doneIterations, acc.doneIterations)
+
+				fmt.Println(exp.totalDuration, acc.totalDuration-flakiness, acc.totalDuration+flakiness)
+				ok := exp.totalDuration > acc.totalDuration-flakiness && exp.totalDuration < acc.totalDuration+flakiness
+				testhelpers.Assert(t, ok, "totalDuration don't match the target: (acc: %v, exp: %v)", acc.totalDuration, exp.totalDuration)
+			}
+		})
+	}
+}
