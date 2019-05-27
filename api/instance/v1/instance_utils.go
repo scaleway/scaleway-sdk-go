861ad720267bea6e20f7ce694fcca5775f1f9a69
DenBeke <DenBeke@users.noreply.github.com>
2019-05-27 11:47:25 +0200

    feat: instance attach/detach volume (#58)

--- api/instance/v1/instance_utils.go
+++ api/instance/v1/instance_utils.go
@@ -1,6 +1,8 @@
 package instance
 
 import (
+	"fmt"
+
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/scaleway/scaleway-sdk-go/utils"
 )
@@ -85,4 +87,121 @@ func (s *API) UpdateIP(req *UpdateIPRequest, opts ...scw.RequestOption) (*Update
 	}
 
 	return &UpdateIPResponse{IP: ipResponse.IP}, nil
+}
+
+// AttachVolumeRequest contains the parameters to attach a volume to a server
+type AttachVolumeRequest struct {
+	Zone     utils.Zone `json:"-"`
+	ServerID string     `json:"-"`
+	VolumeID string     `json:"-"`
+}
+
+// AttachVolumeResponse contains the updated server after attaching a volume
+type AttachVolumeResponse struct {
+	Server *Server `json:"-"`
+}
+
+// volumesToVolumeTemplates converts a map of *Volume to a map of *VolumeTemplate
+// so it can be used in a UpdateServer request
+func volumesToVolumeTemplates(volumes map[string]*Volume) map[string]*VolumeTemplate {
+	volumeTemplates := map[string]*VolumeTemplate{}
+	for key, volume := range volumes {
+		volumeTemplates[key] = &VolumeTemplate{ID: volume.ID, Name: volume.Name}
+	}
+	return volumeTemplates
+}
+
+// AttachVolume attaches a volume to a server
+func (s *API) AttachVolume(req *AttachVolumeRequest, opts ...scw.RequestOption) (*AttachVolumeResponse, error) {
+	// get server with volumes
+	getServerResponse, err := s.GetServer(&GetServerRequest{
+		Zone:     req.Zone,
+		ServerID: req.ServerID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	volumes := getServerResponse.Server.Volumes
+
+	newVolumes := volumesToVolumeTemplates(volumes)
+
+	// add volume to volumes list
+	key := fmt.Sprintf("%d", len(volumes))
+	newVolumes[key] = &VolumeTemplate{
+		ID: req.VolumeID,
+		// name is ignored on this PATCH
+		Name: req.VolumeID,
+	}
+
+	// update server
+	updateServerResponse, err := s.UpdateServer(&UpdateServerRequest{
+		Zone:     req.Zone,
+		ServerID: req.ServerID,
+		Volumes:  &newVolumes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &AttachVolumeResponse{Server: updateServerResponse.Server}, nil
+}
+
+// DetachVolumeRequest contains the parameters to detach a volume from a server
+type DetachVolumeRequest struct {
+	Zone     utils.Zone `json:"-"`
+	VolumeID string     `json:"-"`
+}
+
+// DetachVolumeResponse contains the updated server after detaching a volume
+type DetachVolumeResponse struct {
+	Server *Server `json:"-"`
+}
+
+// DetachVolume detaches a volume from a server
+func (s *API) DetachVolume(req *DetachVolumeRequest, opts ...scw.RequestOption) (*DetachVolumeResponse, error) {
+	// get volume
+	getVolumeResponse, err := s.GetVolume(&GetVolumeRequest{
+		Zone:     req.Zone,
+		VolumeID: req.VolumeID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if getVolumeResponse.Volume == nil {
+		return nil, fmt.Errorf("expected volume to have value in response")
+	}
+	if getVolumeResponse.Volume.Server == nil {
+		return nil, fmt.Errorf("server should be attached to a server")
+	}
+	serverID := getVolumeResponse.Volume.Server.ID
+
+	// get server with volumes
+	getServerResponse, err := s.GetServer(&GetServerRequest{
+		Zone:     req.Zone,
+		ServerID: serverID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	volumes := getServerResponse.Server.Volumes
+	// remove volume from volumes list
+	for key, volume := range volumes {
+		if volume.ID == req.VolumeID {
+			delete(volumes, key)
+		}
+	}
+
+	newVolumes := volumesToVolumeTemplates(volumes)
+
+	// update server
+	updateServerResponse, err := s.UpdateServer(&UpdateServerRequest{
+		Zone:     req.Zone,
+		ServerID: serverID,
+		Volumes:  &newVolumes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &DetachVolumeResponse{Server: updateServerResponse.Server}, nil
 }
--- api/instance/v1/instance_utils_test.go
+++ api/instance/v1/instance_utils_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/scaleway/scaleway-sdk-go/utils"
 )
 
-func TestAttachIP(t *testing.T) {
+func TestInstanceHelpers(t *testing.T) {
 
 	client, r, err := httprecorder.CreateRecordedScwClient("utils-test")
 	testhelpers.Ok(t, err)
@@ -28,93 +28,126 @@ func TestAttachIP(t *testing.T) {
 		reverse      = "1.1.1.1"
 	)
 
-	// Create server
-	createServerResponse, err := instanceAPI.CreateServer(&CreateServerRequest{
-		Zone:         zone,
-		Name:         "instance_utils_test",
-		Organization: organization,
-		Image:        image,
-	})
-	testhelpers.Ok(t, err)
-	serverID = createServerResponse.Server.ID
-	for _, volume := range createServerResponse.Server.Volumes {
-		volumeID = volume.ID
-	}
-
-	// Create IP
-	createIPResponse, err := instanceAPI.CreateIP(&CreateIPRequest{
-		Zone:         zone,
-		Organization: organization,
-	})
-	testhelpers.Ok(t, err)
-	ipID = createIPResponse.IP.ID
-
-	// Attach IP
-	ipAttachResponse, err := instanceAPI.AttachIP(&AttachIPRequest{
-		IPID:     ipID,
-		Zone:     zone,
-		ServerID: serverID,
+	t.Run("create server", func(t *testing.T) {
+		createServerResponse, err := instanceAPI.CreateServer(&CreateServerRequest{
+			Zone:         zone,
+			Name:         "instance_utils_test",
+			Organization: organization,
+			Image:        image,
+		})
+		testhelpers.Ok(t, err)
+		serverID = createServerResponse.Server.ID
+		for _, volume := range createServerResponse.Server.Volumes {
+			volumeID = volume.ID
+		}
 	})
 
-	testhelpers.Ok(t, err)
-	testhelpers.Equals(t, serverID, ipAttachResponse.IP.Server.ID)
+	t.Run("test ip related functions", func(t *testing.T) {
 
-	// Detach IP
-	ipDetachResponse, err := instanceAPI.DetachIP(&DetachIPRequest{
-		IPID: ipID,
-		Zone: zone,
+		// Create IP
+		createIPResponse, err := instanceAPI.CreateIP(&CreateIPRequest{
+			Zone:         zone,
+			Organization: organization,
+		})
+		testhelpers.Ok(t, err)
+		ipID = createIPResponse.IP.ID
+
+		// Attach IP
+		ipAttachResponse, err := instanceAPI.AttachIP(&AttachIPRequest{
+			IPID:     ipID,
+			Zone:     zone,
+			ServerID: serverID,
+		})
+
+		testhelpers.Ok(t, err)
+		testhelpers.Equals(t, serverID, ipAttachResponse.IP.Server.ID)
+
+		// Detach IP
+		ipDetachResponse, err := instanceAPI.DetachIP(&DetachIPRequest{
+			IPID: ipID,
+			Zone: zone,
+		})
+
+		testhelpers.Ok(t, err)
+		testhelpers.Assert(t, nil == ipDetachResponse.IP.Server, "Server object should be nil for detached IP.")
+
+		// Set reverse
+		ipSetReverseResponse, err := instanceAPI.UpdateIP(&UpdateIPRequest{
+			IPID:    ipID,
+			Zone:    zone,
+			Reverse: &reverse,
+		})
+		testhelpers.Ok(t, err)
+		testhelpers.Equals(t, reverse, ipSetReverseResponse.IP.Reverse)
+
+		// Omitempty reverse
+		ipSetReverseResponse, err = instanceAPI.UpdateIP(&UpdateIPRequest{
+			IPID:    ipID,
+			Zone:    zone,
+			Reverse: nil,
+		})
+		testhelpers.Ok(t, err)
+		testhelpers.Equals(t, reverse, ipSetReverseResponse.IP.Reverse)
+
+		// Unset reverse
+		emptyReverse := ""
+		ipDeleteReverseResponse, err := instanceAPI.UpdateIP(&UpdateIPRequest{
+			IPID:    ipID,
+			Zone:    zone,
+			Reverse: &emptyReverse,
+		})
+		testhelpers.Ok(t, err)
+		testhelpers.Equals(t, "", ipDeleteReverseResponse.IP.Reverse)
+
+		// Delete IP
+		err = instanceAPI.DeleteIP(&DeleteIPRequest{
+			Zone: zone,
+			IPID: ipID,
+		})
+		testhelpers.Ok(t, err)
+
 	})
 
-	testhelpers.Ok(t, err)
-	testhelpers.Assert(t, nil == ipDetachResponse.IP.Server, "Server object should be nil for detached IP.")
+	t.Run("Test attach and detach volume", func(t *testing.T) {
 
-	// Set reverse
-	ipSetReverseResponse, err := instanceAPI.UpdateIP(&UpdateIPRequest{
-		IPID:    ipID,
-		Zone:    zone,
-		Reverse: &reverse,
-	})
-	testhelpers.Ok(t, err)
-	testhelpers.Equals(t, reverse, ipSetReverseResponse.IP.Reverse)
+		detachVolumeResponse, err := instanceAPI.DetachVolume(&DetachVolumeRequest{
+			Zone:     zone,
+			VolumeID: volumeID,
+		})
+		testhelpers.Ok(t, err)
 
-	// Omitempty reverse
-	ipSetReverseResponse, err = instanceAPI.UpdateIP(&UpdateIPRequest{
-		IPID:    ipID,
-		Zone:    zone,
-		Reverse: nil,
-	})
-	testhelpers.Ok(t, err)
-	testhelpers.Equals(t, reverse, ipSetReverseResponse.IP.Reverse)
+		testhelpers.Assert(t, detachVolumeResponse.Server != nil, "Should have server in response")
+		testhelpers.Assert(t, detachVolumeResponse.Server.Volumes != nil, "Should have volumes in response")
+		testhelpers.Assert(t, len(detachVolumeResponse.Server.Volumes) == 0, "Server should have zero volumes after detaching")
 
-	// Unset reverse
-	emptyReverse := ""
-	ipDeleteReverseResponse, err := instanceAPI.UpdateIP(&UpdateIPRequest{
-		IPID:    ipID,
-		Zone:    zone,
-		Reverse: &emptyReverse,
-	})
-	testhelpers.Ok(t, err)
-	testhelpers.Equals(t, "", ipDeleteReverseResponse.IP.Reverse)
+		attachVolumeResponse, err := instanceAPI.AttachVolume(&AttachVolumeRequest{
+			Zone:     zone,
+			ServerID: serverID,
+			VolumeID: volumeID,
+		})
+		testhelpers.Ok(t, err)
 
-	// Delete IP
-	err = instanceAPI.DeleteIP(&DeleteIPRequest{
-		Zone: zone,
-		IPID: ipID,
+		testhelpers.Assert(t, attachVolumeResponse.Server != nil, "Should have server in response")
+		testhelpers.Assert(t, attachVolumeResponse.Server.Volumes != nil, "Should have volumes in response")
+		testhelpers.Assert(t, len(attachVolumeResponse.Server.Volumes) == 1, "Server should have one volumes after attaching")
+		testhelpers.Equals(t, volumeID, attachVolumeResponse.Server.Volumes["0"].ID)
 	})
-	testhelpers.Ok(t, err)
 
-	// Delete Server
-	err = instanceAPI.DeleteServer(&DeleteServerRequest{
-		Zone:     zone,
-		ServerID: serverID,
-	})
-	testhelpers.Ok(t, err)
+	t.Run("teardown: delete server and volume", func(t *testing.T) {
+		// Delete Server
+		err = instanceAPI.DeleteServer(&DeleteServerRequest{
+			Zone:     zone,
+			ServerID: serverID,
+		})
+		testhelpers.Ok(t, err)
 
-	// Delete Volume
-	err = instanceAPI.DeleteVolume(&DeleteVolumeRequest{
-		Zone:     zone,
-		VolumeID: volumeID,
+		// Delete Volume
+		err = instanceAPI.DeleteVolume(&DeleteVolumeRequest{
+			Zone:     zone,
+			VolumeID: volumeID,
+		})
+		testhelpers.Ok(t, err)
+
 	})
-	testhelpers.Ok(t, err)
 
 }
