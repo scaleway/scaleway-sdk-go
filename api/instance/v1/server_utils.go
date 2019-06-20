68d8165d922fc07f9b0c81a80a0e73da9ccab973
Quentin Brosse <quentin.brosse@icloud.com>
2019-06-20 20:21:44 +0200

    feat: instance.{Get|SetServerUserData}

--- api/instance/v1/server_utils.go
+++ api/instance/v1/server_utils.go
@@ -1,6 +1,11 @@
 package instance
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/scaleway/scaleway-sdk-go/internal/async"
@@ -126,4 +131,110 @@ func (s *API) GetServerType(req *GetServerTypeRequest) (*ServerType, error) {
 	}
 
 	return nil, errors.New("could not find server type %q", req.Name)
+}
+
+// GetServerUserDataRequest is used by GetServerType method.
+type GetServerUserDataRequest struct {
+	Zone     utils.Zone `json:"-"`
+	ServerID string     `json:"-"`
+
+	// Key define the user data key to get
+	Key string `json:"-"`
+}
+
+// GetServerUserData get user data
+//
+// Get the content of a user data with the given key on a server
+func (s *API) GetServerUserData(req *GetServerUserDataRequest, opts ...scw.RequestOption) (io.ReadCloser, error) {
+	var err error
+
+	if req.Zone == "" {
+		defaultZone, _ := s.client.GetDefaultZone()
+		req.Zone = defaultZone
+	}
+
+	if fmt.Sprint(req.Zone) == "" {
+		return nil, errors.New("field Zone cannot be empty in request")
+	}
+
+	if fmt.Sprint(req.ServerID) == "" {
+		return nil, errors.New("field ServerID cannot be empty in request")
+	}
+
+	if fmt.Sprint(req.Key) == "" {
+		return nil, errors.New("field Key cannot be empty in request")
+	}
+
+	scwReq := &scw.ScalewayRequest{
+		Method:  "GET",
+		Path:    "/instance/v1/zones/" + fmt.Sprint(req.Zone) + "/servers/" + fmt.Sprint(req.ServerID) + "/user_data/" + fmt.Sprint(req.Key),
+		Headers: http.Header{},
+	}
+
+	buffer := &bytes.Buffer{}
+
+	err = s.client.Do(scwReq, buffer, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := ioutil.NopCloser(buffer)
+	return res, nil
+}
+
+type SetServerUserDataRequest struct {
+	Zone     utils.Zone `json:"-"`
+	ServerID string     `json:"-"`
+
+	// Key define the user data key to set
+	Key string `json:"-"`
+
+	// Content define the data to set
+	Content io.Reader
+}
+
+// SetServerUserData add/Set user data
+//
+// Add or update a user data with the given key on a server
+func (s *API) SetServerUserData(req *SetServerUserDataRequest, opts ...scw.RequestOption) error {
+	var err error
+
+	if req.Zone == "" {
+		defaultZone, _ := s.client.GetDefaultZone()
+		req.Zone = defaultZone
+	}
+
+	if fmt.Sprint(req.Zone) == "" {
+		return errors.New("field Zone cannot be empty in request")
+	}
+
+	if fmt.Sprint(req.ServerID) == "" {
+		return errors.New("field ServerID cannot be empty in request")
+	}
+
+	if fmt.Sprint(req.Key) == "" {
+		return errors.New("field Key cannot be empty in request")
+	}
+
+	if req.Content == nil {
+		return errors.New("field Content cannot be nil in request")
+	}
+
+	scwReq := &scw.ScalewayRequest{
+		Method:  "PATCH",
+		Path:    "/instance/v1/zones/" + fmt.Sprint(req.Zone) + "/servers/" + fmt.Sprint(req.ServerID) + "/user_data/" + fmt.Sprint(req.Key),
+		Headers: http.Header{},
+	}
+
+	err = scwReq.SetBody(req.Content)
+	if err != nil {
+		return err
+	}
+
+	err = s.client.Do(scwReq, nil, opts...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
