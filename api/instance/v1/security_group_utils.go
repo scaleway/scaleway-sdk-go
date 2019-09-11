6f85b3c08bdba423507513955239a4b0461fe77d
Jerome Quere <jquere@scaleway.com>
2019-09-11 14:29:04 +0200

    address comments

--- api/instance/v1/security_group_utils.go
+++ api/instance/v1/security_group_utils.go
@@ -103,9 +103,12 @@ type UpdateSecurityGroupRuleRequest struct {
 	IPRange   *string                     `json:"ip_range"`
 	Position  *uint32                     `json:"position,omitempty"`
 
-	// If set to 0 DestPortFrom will be remove
+	// If set to 0, DestPortFrom will be removed.
+	// see SecurityGroupRule.DestPortFrom for more information
 	DestPortFrom *uint32 `json:"dest_port_from"`
-	// If set to 0 DestPortTo will be remove
+
+	// If set to 0, DestPortTo will be removed.
+	// see SecurityGroupRule.DestPortTo for more information
 	DestPortTo *uint32 `json:"dest_port_to"`
 }
 
