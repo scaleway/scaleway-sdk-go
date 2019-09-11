44a986c89d3320ba2b514243fd841b0e4be0e2f2
Olivier Cano <kindermoumoute@users.noreply.github.com>
2019-09-11 15:54:50 +0200

    fix: update version (#180)

--- scw/version.go
+++ scw/version.go
@@ -6,6 +6,6 @@ import (
 )
 
 // TODO: versionning process
-const version = "0.0.0"
+const version = "v1.0.0-beta.1-dev"
 
 var userAgent = fmt.Sprintf("scaleway-sdk-go/%s (%s; %s; %s)", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
