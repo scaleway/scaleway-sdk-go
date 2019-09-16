5342f64b2eda1d3d81d27517ca31788b4e3268e6
Quentin Brosse <quentin.brosse@icloud.com>
2019-09-16 11:42:57 +0200

    Release v1.0.0-beta.2

--- scw/version.go
+++ scw/version.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-// TODO: versionning process
-const version = "v1.0.0-beta.1-dev"
+// TODO: versioning process
+const version = "v1.0.0-beta.2"
 
 var userAgent = fmt.Sprintf("scaleway-sdk-go/%s (%s; %s; %s)", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
