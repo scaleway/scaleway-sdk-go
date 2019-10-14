0026c37d6e478e554ac00d5c867581b7e85acd1b
Jerome Quere <jquere@scaleway.com>
2019-10-14 15:25:26 +0200

    address comments

--- scripts/release/release.js
+++ scripts/release/release.js
@@ -1,11 +1,9 @@
-#!/bin/env node
 const { spawnSync } = require("child_process"),
-    readline = require("readline"),
-    fs = require("fs")
+  readline = require("readline"),
+  fs = require("fs")
 ;
 
-const
-  gitRawCommits = require("git-raw-commits"),
+const gitRawCommits = require("git-raw-commits"),
   semver = require("semver"),
   getStream = require("get-stream"),
   _ = require("colors")
@@ -16,54 +14,28 @@ const _scopeReg = /(\((?<scope>.*)\))?/;
 const _messageReg = /(?<message>[^(]*)/;
 const _mrReg = /(\(#(?<mr>[0-9]+)\))?/;
 const COMMIT_REGEX = new RegExp(`${_typeReg.source}${_scopeReg.source}: *${_messageReg.source} *${_mrReg.source}`);
-const CHANGELOG_PATH = "./CHANGELOG.md";
-const GO_VERSION_PATH = "./scw/version.go";
+const CHANGELOG_PATH = "../../CHANGELOG.md";
+const GO_VERSION_PATH = "../../scw/version.go";
 const TMP_BRANCH = "new-release";
-
-function prompt(prompt) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  return new Promise(resolve => {
-    rl.question(prompt, answer => {
-      resolve(answer);
-      rl.close();
-    });
-  });
-}
-
-function execSync(cmd, args) {
-  console.log(`    ${cmd} ${args.join(" ")}`.grey);
-  const { stdout, status, stderr } = spawnSync(cmd, args, { encoding: "utf8" });
-  if (status !== 0) {
-    throw new Error(`return status ${status}\n${stderr}\n`);
-  }
-  return stdout;
-}
-
-function replaceInFile(path, oldStr, newStr) {
-  console.log(`    Editing ${path}`.grey);
-  const content = fs.readFileSync(path, { encoding: "utf8" }).replace(oldStr, newStr);
-  fs.writeFileSync(path, content);
-}
+const TMP_REMOTE = "scaleway-release";
+const REPO_URL = "git@github.com:scaleway/scaleway-sdk-go.git";
 
 async function main() {
 
   //
   // Initialization
   //
+  console.log("Adding temporary remote on local repo".blue);
+  git( "remote", "add", TMP_REMOTE, REPO_URL);
+  console.log(`   Successfully created ${TMP_REMOTE} remote`.green);
 
-  console.log("Trying to guess gihub username from git remote origin url (only work with ssh)".blue);
-  const githubUsername = execSync("git", ["remote", "get-url", "origin"]).match(/.*:([a-z0-9-]+)\/.*/)[1];
-  console.log(`    We found username: ${githubUsername}`.green);
-
-  console.log("Check that remote with name upstream is valid".blue);
-  execSync("git", ["remote", "get-url", "upstream"]);
-  console.log("    Remote with name upstream exist".green);
+  console.log("Make sure we are working on an up to date master".blue);
+  git( "fetch", TMP_REMOTE);
+  git( "checkout", `${TMP_REMOTE}/master`);
+  console.log(`   Successfully created ${TMP_REMOTE} remote`.green);
 
   console.log("Trying to find last release tag".blue);
-  const lastSemverTag = execSync("git", ["tag"])
+  const lastSemverTag = git("tag")
     .trim()
     .split("\n")
     .filter(semver.valid)
@@ -120,18 +92,12 @@ async function main() {
   await prompt(`Please review ${CHANGELOG_PATH} and ${GO_VERSION_PATH}. When everything is fine hit enter to continue ...`.magenta);
 
   console.log(`Creating release commit`.blue);
-  execSync("git", ["checkout", "-b", TMP_BRANCH]);
-  execSync("git", ["add", CHANGELOG_PATH, GO_VERSION_PATH]);
-  execSync("git", ["commit", "-m", `chore: release ${newVersion}`]);
-  execSync("git", ["push", "-f", "--set-upstream", "origin", TMP_BRANCH]);
-  execSync("git", ["checkout", "master"]);
-  execSync("git", ["branch", "-D", TMP_BRANCH]);
+  git("checkout", "-b", TMP_BRANCH);
+  git("add", CHANGELOG_PATH, GO_VERSION_PATH);
+  git("commit", "-m", `chore: release ${newVersion}`);
+  git("push", "-f", "--set-upstream", TMP_REMOTE, TMP_BRANCH);
 
-  await prompt(`Please create an PR here: https://github.com/${githubUsername}/scaleway-sdk-go/pull/new/new-release . Hit enter when its merged .....`.magenta);
-
-  console.log("Make sure we pull the commit from upstream/master".blue);
-  execSync("git", ["pull", "upstream", "master"]);
-  console.log("    Successfully pull upstream/master".green);
+  await prompt(`Please create an PR here: https://github.com/scaleway/scaleway-sdk-go/pull/new/new-release . Hit enter when its merged .....`.magenta);
 
   console.log("Time to create a github release with the following info\n".blue);
   console.log(`Title: v${newVersion}\n\n`.gray);
@@ -141,22 +107,59 @@ async function main() {
   //
   // Creating post release commit
   //
+  console.log("Make sure we pull the latest commit from master".blue);
+  git("fetch", TMP_REMOTE);
+  git("checkout", `${TMP_REMOTE}/master`);
+  console.log("    Successfully checkout upstream/master".green);
 
   console.log(`Creating post release commit`.blue);
-  execSync("git", ["checkout", "-b", TMP_BRANCH]);
+  git("branch", "-D", TMP_BRANCH);
+  git("checkout", "-b", TMP_BRANCH);
   replaceInFile(GO_VERSION_PATH, /const version[^\n]*\n/, `const version = "v${newVersion}+dev"\n`);
-  execSync("git", ["add", GO_VERSION_PATH]);
-  execSync("git", ["commit", "-m", `chore: post release commit`]);
-  execSync("git", ["push", "-f", "--set-upstream", "origin", TMP_BRANCH]);
-  execSync("git", ["checkout", "master"]);
-  execSync("git", ["branch", "-D", TMP_BRANCH]);
-  await prompt(`Please create an PR here: https://github.com/${githubUsername}/scaleway-sdk-go/pull/new/new-release . Hit enter when its merged .....`.magenta);
+  git("add", GO_VERSION_PATH);
+  git("commit", "-m", "chore: post release commit");
+  git("push", "-f", "--set-upstream", TMP_REMOTE, TMP_BRANCH);
+  git("checkout", "master");
+  git("branch", "-D", TMP_BRANCH);
+  await prompt(`Please create an PR here: https://github.com/scaleway/scaleway-sdk-go/pull/new/new-release . Hit enter when its merged .....`.magenta);
 
-  console.log("Make sure we pull the commit from upstream/master".blue);
-  execSync("git", ["pull", "upstream", "master"]);
-  console.log("    Successfully pull upstream/master".green);
+  console.log("Make sure we pull the latest commit from master".blue);
+  git("pull", TMP_REMOTE, "master");
+  console.log("    Successfully pull master".green);
+
+  console.log("Remove temporary remote".blue);
+  git("remote", "remove", TMP_REMOTE);
+  console.log("    Successfully remove temporary remote".green);
 
   console.log(`🚀 Release Success `.green);
+}
+
+function git(...args) {
+  console.log(`    git ${args.join(" ")}`.grey);
+  const { stdout, status, stderr } = spawnSync("git", args, { encoding: "utf8" });
+  if (status !== 0) {
+    throw new Error(`return status ${status}\n${stderr}\n`);
+  }
+  return stdout;
+}
+
+function replaceInFile(path, oldStr, newStr) {
+  console.log(`    Editing ${path}`.grey);
+  const content = fs.readFileSync(path, { encoding: "utf8" }).replace(oldStr, newStr);
+  fs.writeFileSync(path, content);
+}
+
+function prompt(prompt) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  return new Promise(resolve => {
+    rl.question(prompt, answer => {
+      resolve(answer);
+      rl.close();
+    });
+  });
 }
 
 main().catch(console.error);
