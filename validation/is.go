7e9b27f3abd75ced9da653789f943f1eb5825b9f
Quentin Brosse <qbrosse@scaleway.com>
2019-11-15 12:02:23 +0100

    feat(validation): add IsEmail

--- validation/is.go
+++ validation/is.go
@@ -7,10 +7,11 @@ import (
 )
 
 var (
-	isUUIDRegexp  = regexp.MustCompile(`[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}`)
+	isUUIDRegexp  = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
 	isRegionRegex = regexp.MustCompile("^[a-z]{2}-[a-z]{3}$")
 	isZoneRegex   = regexp.MustCompile("^[a-z]{2}-[a-z]{3}-[1-9]$")
 	isAccessKey   = regexp.MustCompile("^SCW[A-Z0-9]{17}$")
+	isEmailRegexp = regexp.MustCompile("^.+@.+$")
 )
 
 // IsUUID returns true if the given string has a valid UUID format.
@@ -47,4 +48,9 @@ func IsZone(s string) bool {
 func IsURL(s string) bool {
 	_, err := url.Parse(s)
 	return err == nil
+}
+
+// IsEmail returns true if the given string has an email format.
+func IsEmail(v string) bool {
+	return isEmailRegexp.MatchString(v)
 }
