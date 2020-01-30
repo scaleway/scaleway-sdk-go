819ad04fb47eec6ef1148efcadce5280fbf2deda
Olivier CANO <ocano@scaleway.com>
2020-01-30 15:17:34 +0100

    feat(scw): GetCacheDirectory

--- scw/path.go
+++ scw/path.go
@@ -10,6 +10,7 @@ const (
 	unixHomeDirEnv    = "HOME"
 	windowsHomeDirEnv = "USERPROFILE"
 	xdgConfigDirEnv   = "XDG_CONFIG_HOME"
+	xdgCacheDirEnv    = "XDG_CACHE_HOME"
 
 	defaultConfigFileName = "config.yaml"
 )
@@ -18,6 +19,31 @@ var (
 	// ErrNoHomeDir errors when no user directory is found
 	ErrNoHomeDir = errors.New("user home directory not found")
 )
+
+// GetCacheDirectory returns the default path.
+// Default path is base on the following priority order:
+// - $SCW_CACHE_DIR
+// - $XDG_CACHE_HOME/scw
+// - $HOME/.cache/scw
+// - $USERPROFILE/.cache/scw
+func GetCacheDirectory() string {
+	cacheDir := ""
+	switch {
+	case os.Getenv(scwCacheDirEnv) != "":
+		cacheDir = os.Getenv(scwCacheDirEnv)
+	case os.Getenv(xdgCacheDirEnv) != "":
+		cacheDir = filepath.Join(os.Getenv(xdgCacheDirEnv), "scw")
+	case os.Getenv(unixHomeDirEnv) != "":
+		cacheDir = filepath.Join(os.Getenv(unixHomeDirEnv), ".cache", "scw")
+	case os.Getenv(windowsHomeDirEnv) != "":
+		cacheDir = filepath.Join(os.Getenv(windowsHomeDirEnv), ".cache", "scw")
+	default:
+		// TODO: fallback on local folder?
+	}
+
+	// Clean the cache directory path when exiting the function
+	return filepath.Clean(cacheDir)
+}
 
 // GetConfigPath returns the default path.
 // Default path is base on the following priority order:
@@ -33,7 +59,7 @@ func GetConfigPath() string {
 	return filepath.Clean(configPath)
 }
 
-// getConfigV2FilePath returns the path to the Scaleway CLI config file
+// getConfigV2FilePath returns the path to the v2 config file
 func getConfigV2FilePath() (string, bool) {
 	configDir, err := getScwConfigDir()
 	if err != nil {
@@ -42,7 +68,7 @@ func getConfigV2FilePath() (string, bool) {
 	return filepath.Clean(filepath.Join(configDir, defaultConfigFileName)), true
 }
 
-// getConfigV1FilePath returns the path to the Scaleway CLI config file
+// getConfigV1FilePath returns the path to the v1 config file
 func getConfigV1FilePath() (string, bool) {
 	path, err := getHomeDir()
 	if err != nil {
