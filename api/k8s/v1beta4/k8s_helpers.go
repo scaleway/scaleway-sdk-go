d26e180da05a901df9104b7601f429d40d3903e9
Mathieu Debove <debovema@users.noreply.github.com>
2020-02-18 13:39:49 +0100

    feat(k8s): add WaitForPoolNodesReady & WaitForClusterNodesReady helper methods (#312)

--- api/k8s/v1beta4/k8s_helpers.go
+++ api/k8s/v1beta4/k8s_helpers.go
@@ -10,6 +10,7 @@ import (
 
 const (
 	waitForClusterDefaultTimeout = time.Minute * 15
+	waitForPoolDefaultTimeout    = time.Minute * 15
 )
 
 // WaitForClusterRequest is used by WaitForCluster method.
@@ -54,4 +55,80 @@ func (s *API) WaitForCluster(req *WaitForClusterRequest) (*Cluster, error) {
 		return nil, errors.Wrap(err, "waiting for cluster failed")
 	}
 	return cluster.(*Cluster), nil
+}
+
+// WaitForClusterPoolsRequest is used by WaitForClusterPools method.
+type WaitForClusterPoolsRequest struct {
+	ClusterID string
+	Region    scw.Region
+	Timeout   *time.Duration
+}
+
+// WaitForClusterPools waits for the pools of a cluster to be ready
+func (s *API) WaitForClusterPools(req *WaitForClusterPoolsRequest) error {
+	timeout := waitForPoolDefaultTimeout
+	if req.Timeout != nil {
+		timeout = *req.Timeout
+	}
+
+	pools, err := s.ListPools(&ListPoolsRequest{
+		ClusterID: req.ClusterID,
+		Region:    req.Region,
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, pool := range pools.Pools {
+		err = s.WaitForPool(&WaitForPoolRequest{
+			PoolID:  pool.ID,
+			Timeout: &timeout,
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// WaitForPoolRequest is used by WaitForPool method.
+type WaitForPoolRequest struct {
+	PoolID  string
+	Region  scw.Region
+	Timeout *time.Duration
+}
+
+// WaitForPool waits for a pool to be ready
+func (s *API) WaitForPool(req *WaitForPoolRequest) error {
+	terminalStatus := map[PoolStatus]struct{}{
+		PoolStatusReady:   {},
+		PoolStatusWarning: {},
+	}
+
+	timeout := waitForPoolDefaultTimeout
+	if req.Timeout != nil {
+		timeout = *req.Timeout
+	}
+
+	_, err := async.WaitSync(&async.WaitSyncConfig{
+		Get: func() (interface{}, bool, error) {
+			res, err := s.GetPool(&GetPoolRequest{
+				PoolID: req.PoolID,
+				Region: req.Region,
+			})
+
+			if err != nil {
+				return nil, false, err
+			}
+			_, isTerminal := terminalStatus[res.Status]
+
+			return nil, isTerminal, nil
+		},
+		Timeout:          timeout,
+		IntervalStrategy: async.LinearIntervalStrategy(5 * time.Second),
+	})
+
+	return err
 }
