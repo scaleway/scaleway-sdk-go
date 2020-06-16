6f25dabd0ee78eb61fdf5f21c957a07b0409da77
Clément Decoodt <cdecoodt@online.net>
2020-06-16 09:48:02 +0200

    fix(sdk_compilation_test): add missing api packages in test
    Signed-off-by: Clément Decoodt <cdecoodt@online.net>

--- sdk_compilation_test.go
+++ sdk_compilation_test.go
@@ -3,6 +3,18 @@ package scalewaysdkgo
 // This test file makes sure that all the auto-generated code compiles.
 
 import (
+	_ "github.com/scaleway/scaleway-sdk-go/api/account/v2alpha1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1alpha1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/domain/v2alpha2"
 	_ "github.com/scaleway/scaleway-sdk-go/api/instance/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/iot/v1beta1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1beta3"
+	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1beta4"
 	_ "github.com/scaleway/scaleway-sdk-go/api/lb/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/marketplace/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/registry/v1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/test/v1"
 )
