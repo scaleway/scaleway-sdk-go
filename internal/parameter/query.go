abc4aeb2a4e61db6a849861578e4b17c209a4b7c
Jerome Quere <jquere@scaleway.com>
2020-07-07 15:05:22 +0200

    fix: date format in query parameters (#471)

--- internal/parameter/query.go
+++ internal/parameter/query.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"reflect"
+	"time"
 )
 
 // AddToQuery add a key/value pair to an URL query
@@ -24,6 +25,8 @@ func AddToQuery(query url.Values, key string, value interface{}) {
 		for i := 0; i < elemValue.Len(); i++ {
 			query.Add(key, fmt.Sprint(elemValue.Index(i).Interface()))
 		}
+	case elemType == reflect.TypeOf(time.Time{}):
+		query.Add(key, value.(time.Time).Format(time.RFC3339))
 	default:
 		query.Add(key, fmt.Sprint(elemValue.Interface()))
 	}
