d82a46462d002a06f163dd9857955c83c34fcb7b
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2021-11-09 10:56:19 +0100

    feat(lb): update wait for all private networks on a load balancer

--- api/lb/v1/lb_utils.go
+++ api/lb/v1/lb_utils.go
@@ -91,13 +91,12 @@ func waitForLb(timeout *time.Duration, retryInterval *time.Duration, getLB func(
 // ZonedAPIWaitForLBPNRequest is used by WaitForLBPN method.
 type ZonedAPIWaitForLBPNRequest struct {
 	LBID          string
-	PNID          string
 	Zone          scw.Zone
 	Timeout       *time.Duration
 	RetryInterval *time.Duration
 }
 
-func waitForPNLb(timeout *time.Duration, retryInterval *time.Duration, getPN func() (*PrivateNetwork, error)) (*PrivateNetwork, error) {
+func waitForPNLb(timeout *time.Duration, retryInterval *time.Duration, getPNs func() ([]*PrivateNetwork, error)) ([]*PrivateNetwork, error) {
 	t := defaultTimeout
 	if timeout != nil {
 		t = *timeout
@@ -114,14 +113,19 @@ func waitForPNLb(timeout *time.Duration, retryInterval *time.Duration, getPN fun
 
 	pn, err := async.WaitSync(&async.WaitSyncConfig{
 		Get: func() (interface{}, bool, error) {
-			res, err := getPN()
+			pns, err := getPNs()
 
-			if err != nil {
-				return nil, false, err
+			for _, pn := range pns {
+				if err != nil {
+					return nil, false, err
+				}
+				//wait at the first not terminal state
+				_, isTerminal := terminalStatus[pn.Status]
+				if !isTerminal {
+					return pns, isTerminal, nil
+				}
 			}
-			_, isTerminal := terminalStatus[res.Status]
-
-			return res, isTerminal, nil
+			return pns, true, nil
 		},
 		Timeout:          t,
 		IntervalStrategy: async.LinearIntervalStrategy(r),
@@ -129,14 +133,14 @@ func waitForPNLb(timeout *time.Duration, retryInterval *time.Duration, getPN fun
 	if err != nil {
 		return nil, errors.Wrap(err, "waiting for attachment failed")
 	}
-	return pn.(*PrivateNetwork), nil
+	return pn.([]*PrivateNetwork), nil
 }
 
 // WaitForLBPN waits for the private_network attached status on a load balancer
 // to be in a "terminal state" before returning.
 // This function can be used to wait for an attached private_network to be ready for example.
-func (s *ZonedAPI) WaitForLBPN(req *ZonedAPIWaitForLBPNRequest, opts ...scw.RequestOption) (*PrivateNetwork, error) {
-	return waitForPNLb(req.Timeout, req.RetryInterval, func() (*PrivateNetwork, error) {
+func (s *ZonedAPI) WaitForLBPN(req *ZonedAPIWaitForLBPNRequest, opts ...scw.RequestOption) ([]*PrivateNetwork, error) {
+	return waitForPNLb(req.Timeout, req.RetryInterval, func() ([]*PrivateNetwork, error) {
 		lbPNs, err := s.ListLBPrivateNetworks(&ZonedAPIListLBPrivateNetworksRequest{
 			Zone: req.Zone,
 			LBID: req.LBID,
@@ -145,12 +149,6 @@ func (s *ZonedAPI) WaitForLBPN(req *ZonedAPIWaitForLBPNRequest, opts ...scw.Requ
 			return nil, err
 		}
 
-		for _, pn := range lbPNs.PrivateNetwork {
-			if pn.PrivateNetworkID == req.PNID {
-				return pn, nil
-			}
-		}
-
-		return nil, errors.Wrap(err, "private network not found")
+		return lbPNs.PrivateNetwork, nil
 	})
 }
