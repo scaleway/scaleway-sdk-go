d1fdd2c8a791fc73a89c5fcc0f0877696336e3e8
Rémy Léone <rleone@scaleway.com>
2021-11-16 14:06:24 +0100

    Fix

--- api/rdb/v1/rdb_sdk.go
+++ api/rdb/v1/rdb_sdk.go
@@ -815,8 +815,6 @@ type DatabaseBackup struct {
 	DownloadURLExpiresAt *time.Time `json:"download_url_expires_at"`
 	// Region: region of this database backup
 	Region scw.Region `json:"region"`
-	// SameRegion: store logical backups in the same region as the source database instance
-	SameRegion bool `json:"same_region"`
 }
 
 // DatabaseEngine: database engine
@@ -872,8 +870,6 @@ type EndpointPrivateNetworkDetails struct {
 	PrivateNetworkID string `json:"private_network_id"`
 
 	ServiceIP scw.IPNet `json:"service_ip"`
-
-	Zone scw.Zone `json:"zone"`
 }
 
 type EndpointSpec struct {
@@ -980,10 +976,6 @@ type Instance struct {
 	InitSettings []*InstanceSetting `json:"init_settings"`
 	// Endpoints: list of instance endpoints
 	Endpoints []*Endpoint `json:"endpoints"`
-	// LogsPolicy: logs policy of the instance
-	LogsPolicy *LogsPolicy `json:"logs_policy"`
-	// BackupSameRegion: store logical backups in the same region as the database instance
-	BackupSameRegion bool `json:"backup_same_region"`
 }
 
 // InstanceLog: instance log
@@ -1050,9 +1042,7 @@ type ListInstanceACLRulesResponse struct {
 	TotalCount uint32 `json:"total_count"`
 }
 
-// ListInstanceLogsDetailsResponse: list instance logs details response
 type ListInstanceLogsDetailsResponse struct {
-	// Details: remote instance logs details
 	Details []*ListInstanceLogsDetailsResponseInstanceLogDetail `json:"details"`
 }
 
@@ -1106,14 +1096,6 @@ type ListUsersResponse struct {
 	Users []*User `json:"users"`
 	// TotalCount: total count of users present on a given instance
 	TotalCount uint32 `json:"total_count"`
-}
-
-// LogsPolicy: logs policy
-type LogsPolicy struct {
-	// MaxAgeRetention: max age (in day) of remote logs to keep on the database instance
-	MaxAgeRetention *uint32 `json:"max_age_retention"`
-	// TotalDiskRetention: max disk size of remote logs to keep on the database instance
-	TotalDiskRetention *scw.Size `json:"total_disk_retention"`
 }
 
 // NodeType: node type
@@ -1890,8 +1872,6 @@ type CreateInstanceRequest struct {
 	VolumeSize scw.Size `json:"volume_size"`
 	// InitEndpoints: one or multiple EndpointSpec used to expose your database instance
 	InitEndpoints []*EndpointSpec `json:"init_endpoints"`
-	// BackupSameRegion: store logical backups in the same region as the database instance
-	BackupSameRegion bool `json:"backup_same_region"`
 }
 
 // CreateInstance: create an instance
@@ -1955,10 +1935,6 @@ type UpdateInstanceRequest struct {
 	Name *string `json:"name"`
 	// Tags: tags of a given instance
 	Tags *[]string `json:"tags"`
-	// LogsPolicy: logs policy of the instance
-	LogsPolicy *LogsPolicy `json:"logs_policy"`
-	// BackupSameRegion: store logical backups in the same region as the database instance
-	BackupSameRegion *bool `json:"backup_same_region"`
 }
 
 // UpdateInstance: update an instance
@@ -2123,11 +2099,10 @@ func (s *API) GetInstanceCertificate(req *GetInstanceCertificateRequest, opts ..
 
 type RenewInstanceCertificateRequest struct {
 	Region scw.Region `json:"-"`
-	// InstanceID: UUID of the instance you want logs of
+
 	InstanceID string `json:"-"`
 }
 
-// RenewInstanceCertificate: renew the TLS certificate of an instance
 func (s *API) RenewInstanceCertificate(req *RenewInstanceCertificateRequest, opts ...scw.RequestOption) error {
 	var err error
 
@@ -2349,13 +2324,12 @@ func (s *API) GetInstanceLog(req *GetInstanceLogRequest, opts ...scw.RequestOpti
 
 type PurgeInstanceLogsRequest struct {
 	Region scw.Region `json:"-"`
-	// InstanceID: UUID of the instance you want logs of
+
 	InstanceID string `json:"-"`
-	// LogName: specific log name to purge
+
 	LogName *string `json:"log_name"`
 }
 
-// PurgeInstanceLogs: purge remote instances logs
 func (s *API) PurgeInstanceLogs(req *PurgeInstanceLogsRequest, opts ...scw.RequestOption) error {
 	var err error
 
@@ -2392,11 +2366,10 @@ func (s *API) PurgeInstanceLogs(req *PurgeInstanceLogsRequest, opts ...scw.Reque
 
 type ListInstanceLogsDetailsRequest struct {
 	Region scw.Region `json:"-"`
-	// InstanceID: UUID of the instance you want logs of
+
 	InstanceID string `json:"-"`
 }
 
-// ListInstanceLogsDetails: list remote instances logs details
 func (s *API) ListInstanceLogsDetails(req *ListInstanceLogsDetailsRequest, opts ...scw.RequestOption) (*ListInstanceLogsDetailsResponse, error) {
 	var err error
 
@@ -2624,8 +2597,6 @@ type AddInstanceACLRulesRequest struct {
 }
 
 // AddInstanceACLRules: add an ACL instance to a given instance
-//
-// Add an additional ACL rule to a database instance.
 func (s *API) AddInstanceACLRules(req *AddInstanceACLRulesRequest, opts ...scw.RequestOption) (*AddInstanceACLRulesResponse, error) {
 	var err error
 
@@ -2671,8 +2642,6 @@ type SetInstanceACLRulesRequest struct {
 }
 
 // SetInstanceACLRules: set ACL rules for a given instance
-//
-// Replace all the ACL rules of a database instance.
 func (s *API) SetInstanceACLRules(req *SetInstanceACLRulesRequest, opts ...scw.RequestOption) (*SetInstanceACLRulesResponse, error) {
 	var err error
 
@@ -3513,13 +3482,12 @@ func (s *API) CreateInstanceFromSnapshot(req *CreateInstanceFromSnapshotRequest,
 
 type CreateEndpointRequest struct {
 	Region scw.Region `json:"-"`
-	// InstanceID: UUID of the instance you want to add endpoint to
+
 	InstanceID string `json:"-"`
-	// EndpointSpec: specification of the endpoint you want to create
+
 	EndpointSpec *EndpointSpec `json:"endpoint_spec"`
 }
 
-// CreateEndpoint: add an instance endpoint
 func (s *API) CreateEndpoint(req *CreateEndpointRequest, opts ...scw.RequestOption) (*Endpoint, error) {
 	var err error
 
@@ -3558,11 +3526,10 @@ func (s *API) CreateEndpoint(req *CreateEndpointRequest, opts ...scw.RequestOpti
 
 type DeleteEndpointRequest struct {
 	Region scw.Region `json:"-"`
-	// EndpointID: UUID of the endpoint you want to delete
+
 	EndpointID string `json:"-"`
 }
 
-// DeleteEndpoint: delete an instance endpoint
 func (s *API) DeleteEndpoint(req *DeleteEndpointRequest, opts ...scw.RequestOption) error {
 	var err error
 
@@ -3594,11 +3561,10 @@ func (s *API) DeleteEndpoint(req *DeleteEndpointRequest, opts ...scw.RequestOpti
 
 type GetEndpointRequest struct {
 	Region scw.Region `json:"-"`
-	// EndpointID: UUID of the endpoint you want to get
+
 	EndpointID string `json:"-"`
 }
 
-// GetEndpoint: get an instance endpoint
 func (s *API) GetEndpoint(req *GetEndpointRequest, opts ...scw.RequestOption) (*Endpoint, error) {
 	var err error
 
