d9381557e404962e06dc15e71a490fcd8e3236d6
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-01-13 10:51:50 +0100

    feat(locality): add nl-ams-2 zone

--- scw/locality.go
+++ scw/locality.go
@@ -25,6 +25,8 @@ const (
 	ZoneFrPar3 = Zone("fr-par-3")
 	// ZoneNlAms1 represents the nl-ams-1 zone
 	ZoneNlAms1 = Zone("nl-ams-1")
+	// ZoneNlAms2 represents the nl-ams-2 zone
+	ZoneNlAms2 = Zone("nl-ams-2")
 	// ZonePlWaw1 represents the pl-waw-1 zone
 	ZonePlWaw1 = Zone("pl-waw-1")
 )
@@ -36,6 +38,7 @@ var (
 		ZoneFrPar2,
 		ZoneFrPar3,
 		ZoneNlAms1,
+		ZoneNlAms2,
 		ZonePlWaw1,
 	}
 )
@@ -103,7 +106,7 @@ func (region Region) GetZones() []Zone {
 	case RegionFrPar:
 		return []Zone{ZoneFrPar1, ZoneFrPar2, ZoneFrPar3}
 	case RegionNlAms:
-		return []Zone{ZoneNlAms1}
+		return []Zone{ZoneNlAms1, ZoneNlAms2}
 	case RegionPlWaw:
 		return []Zone{ZonePlWaw1}
 	default:
--- scw/locality_test.go
+++ scw/locality_test.go
@@ -23,6 +23,10 @@ func TestParseZone(t *testing.T) {
 			expected: ZonePlWaw1,
 		},
 		{
+			input:    "nl-ams-2",
+			expected: ZoneNlAms2,
+		},
+		{
 			input:    "par1",
 			expected: ZoneFrPar1,
 		},
@@ -37,17 +41,17 @@ func TestParseZone(t *testing.T) {
 		{
 			input:    "fr-par",
 			expected: "",
-			err:      errors.New("bad zone format, available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, pl-waw-1"),
+			err:      errors.New("bad zone format, available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, nl-ams-2, pl-waw-1"),
 		},
 		{
 			input:    "fr-par-n",
 			expected: "",
-			err:      errors.New("bad zone format, available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, pl-waw-1"),
+			err:      errors.New("bad zone format, available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, nl-ams-2, pl-waw-1"),
 		},
 		{
 			input:    "fr-par-0",
 			expected: "",
-			err:      errors.New("bad zone format, available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, pl-waw-1"),
+			err:      errors.New("bad zone format, available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, nl-ams-2, pl-waw-1"),
 		},
 	}
 
