619f9be4eed9539a342cb53f69aefe623965c73d
Rémy Léone <rleone@scaleway.com>
2022-01-18 15:36:07 +0100

    chore(k8s): remove support for betas versions (#1033)

--- sdk_compilation_test.go
+++ sdk_compilation_test.go
@@ -10,8 +10,6 @@ import (
 	_ "github.com/scaleway/scaleway-sdk-go/api/instance/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/iot/v1beta1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1"
-	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1beta3"
-	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1beta4"
 	_ "github.com/scaleway/scaleway-sdk-go/api/lb/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/marketplace/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
