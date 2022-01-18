b5215f7813966f825ba4f47455ba418cf14e6cac
Rémy Léone <rleone@scaleway.com>
2022-01-18 15:13:36 +0100

    FIx

--- sdk_compilation_test.go
+++ sdk_compilation_test.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/scaleway/scaleway-sdk-go/api/account/v2alpha1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1alpha1"
-	_ "github.com/scaleway/scaleway-sdk-go/api/domain/v2alpha2"
+	_ "github.com/scaleway/scaleway-sdk-go/api/domain/v2beta1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/instance/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/iot/v1beta1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1"
