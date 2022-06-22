76541f63363d6ca3f1de7dc0dccddfbf65705251
Leïla MARABESE <lmarabese@scaleway.com>
2022-06-22 14:51:31 +0200

    add public UpdateImage function (with type pbs)

--- api/instance/v1/image_utils.go
+++ api/instance/v1/image_utils.go
@@ -1,6 +1,7 @@
 package instance
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/scaleway/scaleway-sdk-go/internal/async"
@@ -53,4 +54,91 @@ func (s *API) WaitForImage(req *WaitForImageRequest, opts ...scw.RequestOption) 
 		return nil, errors.Wrap(err, "waiting for image failed")
 	}
 	return image.(*Image), nil
+}
+
+type UpdateImageRequest struct {
+	Zone                scw.Zone
+	ImageID             string
+	Name                *string   `json:"name,omitempty"`
+	RootVolumeID        string    `json:"root_volume_id,omitempty"`
+	Arch                Arch      `json:"architecture,omitempty"`
+	DefaultBootscriptID string    `json:"default_bootscript_id,omitempty"`
+	ExtraVolumesIDs     []string  `json:"additional_volume_ids,omitempty"`
+	Public              bool      `json:"public,omitempty"`
+	Tags                *[]string `json:"tags,omitempty"`
+}
+
+type UpdateImageResponse struct {
+	Image *Image
+}
+
+func (s *API) UpdateImage(req *UpdateImageRequest, opts ...scw.RequestOption) (*UpdateImageResponse, error) {
+	if req.Zone == "" {
+		defaultZone, _ := s.client.GetDefaultZone()
+		req.Zone = defaultZone
+	}
+	if fmt.Sprint(req.Zone) == "" {
+		return nil, errors.New("field Zone cannot be empty in request")
+	}
+	if fmt.Sprint(req.ImageID) == "" {
+		return nil, errors.New("field ImageID cannot be empty in request")
+	}
+
+	getImageResponse, err := s.GetImage(&GetImageRequest{
+		Zone:    req.Zone,
+		ImageID: req.ImageID,
+	}, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	setRequest := &SetImageRequest{
+		Zone:              getImageResponse.Image.Zone,
+		ID:                getImageResponse.Image.ID,
+		Name:              getImageResponse.Image.Name,
+		Arch:              getImageResponse.Image.Arch,
+		CreationDate:      getImageResponse.Image.CreationDate,
+		ModificationDate:  getImageResponse.Image.ModificationDate,
+		DefaultBootscript: getImageResponse.Image.DefaultBootscript,
+		ExtraVolumes:      getImageResponse.Image.ExtraVolumes,
+		FromServer:        getImageResponse.Image.FromServer,
+		Organization:      getImageResponse.Image.Organization,
+		Public:            getImageResponse.Image.Public,
+		RootVolume:        getImageResponse.Image.RootVolume,
+		State:             getImageResponse.Image.State,
+		Project:           getImageResponse.Image.Project,
+		Tags:              &getImageResponse.Image.Tags,
+	}
+
+	// Override the values that need to be updated
+	if req.Name != nil {
+		setRequest.Name = *req.Name
+	}
+	if req.Tags != nil {
+		setRequest.Tags = req.Tags
+	}
+	if req.RootVolumeID != "" {
+		setRequest.RootVolume = req.RootVolumeID
+	}
+	if req.Arch != "" {
+		setRequest.Arch = req.Arch
+	}
+	if req.DefaultBootscriptID != "" {
+		setRequest.DefaultBootscript = req.DefaultBootscriptID
+	}
+	if req.ExtraVolumesIDs != nil {
+		setRequest.ExtraVolumes = req.ExtraVolumesIDs
+	}
+	if req.Public != setRequest.Public {
+		setRequest.Public = req.Public
+	}
+
+	setRes, err := s.setImage(setRequest, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UpdateImageResponse{
+		Image: setRes.Image,
+	}, nil
 }
