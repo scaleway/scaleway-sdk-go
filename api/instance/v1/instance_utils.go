e792c0d04360a8206b7fa27593024237276cd76a
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2022-08-01 15:15:54 +0200

    feat(instance): add wait for mac address on private nic (#1362)

--- api/instance/v1/instance_utils.go
+++ api/instance/v1/instance_utils.go
@@ -336,7 +336,7 @@ func (v *NullableStringValue) MarshalJSON() ([]byte, error) {
 // WaitForPrivateNICRequest is used by WaitForPrivateNIC method.
 type WaitForPrivateNICRequest struct {
 	ServerID      string
-	PrivateNicID string
+	PrivateNicID  string
 	Zone          scw.Zone
 	Timeout       *time.Duration
 	RetryInterval *time.Duration
@@ -355,15 +355,15 @@ func (s *API) WaitForPrivateNIC(req *WaitForPrivateNICRequest, opts ...scw.Reque
 	}
 
 	terminalStatus := map[PrivateNICState]struct{}{
-		PrivateNICStateAvailable:        {},
+		PrivateNICStateAvailable:    {},
 		PrivateNICStateSyncingError: {},
 	}
 
 	pn, err := async.WaitSync(&async.WaitSyncConfig{
 		Get: func() (interface{}, bool, error) {
 			res, err := s.GetPrivateNIC(&GetPrivateNICRequest{
-				ServerID: req.ServerID,
-				Zone:     req.Zone,
+				ServerID:     req.ServerID,
+				Zone:         req.Zone,
 				PrivateNicID: req.PrivateNicID,
 			}, opts...)
 
@@ -373,6 +373,53 @@ func (s *API) WaitForPrivateNIC(req *WaitForPrivateNICRequest, opts ...scw.Reque
 			_, isTerminal := terminalStatus[res.PrivateNic.State]
 
 			return res.PrivateNic, isTerminal, err
+		},
+		Timeout:          timeout,
+		IntervalStrategy: async.LinearIntervalStrategy(retryInterval),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "waiting for server failed")
+	}
+	return pn.(*PrivateNIC), nil
+}
+
+// WaitForMACAddressRequest is used by WaitForMACAddress method.
+type WaitForMACAddressRequest struct {
+	ServerID      string
+	PrivateNicID  string
+	Zone          scw.Zone
+	Timeout       *time.Duration
+	RetryInterval *time.Duration
+}
+
+// WaitForMACAddress wait for the MAC address be assigned on instance before returning.
+// This function can be used to wait for the private network to be attached for example.
+func (s *API) WaitForMACAddress(req *WaitForMACAddressRequest, opts ...scw.RequestOption) (*PrivateNIC, error) {
+	timeout := defaultTimeout
+	if req.Timeout != nil {
+		timeout = *req.Timeout
+	}
+	retryInterval := defaultRetryInterval
+	if req.RetryInterval != nil {
+		retryInterval = *req.RetryInterval
+	}
+
+	pn, err := async.WaitSync(&async.WaitSyncConfig{
+		Get: func() (interface{}, bool, error) {
+			res, err := s.GetPrivateNIC(&GetPrivateNICRequest{
+				ServerID:     req.ServerID,
+				Zone:         req.Zone,
+				PrivateNicID: req.PrivateNicID,
+			}, opts...)
+			if err != nil {
+				return nil, false, err
+			}
+
+			if len(res.PrivateNic.MacAddress) > 0 {
+				return res.PrivateNic, true, err
+			}
+
+			return res.PrivateNic, false, err
 		},
 		Timeout:          timeout,
 		IntervalStrategy: async.LinearIntervalStrategy(retryInterval),
