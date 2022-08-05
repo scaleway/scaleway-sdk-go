61ee3b46cb4b2eb55f34868526f6d0c9cf488153
Jules Castéran <jcasteran@scaleway.com>
2022-08-05 16:29:44 +0200

    feat(rdb): add wait for read replica (#1372)

--- api/rdb/v1/rdb_utils.go
+++ api/rdb/v1/rdb_utils.go
@@ -154,3 +154,51 @@ func (s *API) WaitForInstanceLog(req *WaitForInstanceLogRequest, opts ...scw.Req
 	}
 	return logs.(*InstanceLog), nil
 }
+
+// WaitForReadReplicaRequest is used by WaitForReadReplica method.
+type WaitForReadReplicaRequest struct {
+	ReadReplicaID string
+	Region        scw.Region
+	Timeout       *time.Duration
+	RetryInterval *time.Duration
+}
+
+// WaitForReadReplica waits for the read replica to be in a "terminal state" before returning.
+// This function can be used to wait for a read replica to be ready for example.
+func (s *API) WaitForReadReplica(req *WaitForInstanceRequest, opts ...scw.RequestOption) (*ReadReplica, error) {
+	timeout := defaultTimeout
+	if req.Timeout != nil {
+		timeout = *req.Timeout
+	}
+	retryInterval := defaultRetryInterval
+	if req.RetryInterval != nil {
+		retryInterval = *req.RetryInterval
+	}
+
+	terminalStatus := map[ReadReplicaStatus]struct{}{
+		ReadReplicaStatusReady: {},
+		ReadReplicaStatusError: {},
+	}
+
+	readReplica, err := async.WaitSync(&async.WaitSyncConfig{
+		Get: func() (interface{}, bool, error) {
+			res, err := s.GetReadReplica(&GetReadReplicaRequest{
+				ReadReplicaID: req.InstanceID,
+				Region:        req.Region,
+			}, opts...)
+
+			if err != nil {
+				return nil, false, err
+			}
+			_, isTerminal := terminalStatus[res.Status]
+
+			return res, isTerminal, nil
+		},
+		Timeout:          timeout,
+		IntervalStrategy: async.LinearIntervalStrategy(retryInterval),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "waiting for read replcia failed")
+	}
+	return readReplica.(*ReadReplica), nil
+}
