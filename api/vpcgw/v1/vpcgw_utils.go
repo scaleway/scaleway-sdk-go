be8e35f6816ddd310e3a1f797b0ba526b49df327
Jules Castéran <jcasteran@scaleway.com>
2022-09-07 10:17:45 +0200

    feat(vpcgw): add dhcp entry waiter (#1407)

--- api/vpcgw/v1/vpcgw_utils.go
+++ api/vpcgw/v1/vpcgw_utils.go
@@ -114,3 +114,56 @@ func (s *API) WaitForGatewayNetwork(req *WaitForGatewayNetworkRequest, opts ...s
 
 	return gatewayNetwork.(*GatewayNetwork), nil
 }
+
+// WaitForDHCPEntriesRequest is used by WaitForDHCPEntries method
+type WaitForDHCPEntriesRequest struct {
+	GatewayNetworkID *string
+	MacAddress       string
+
+	Zone          scw.Zone
+	Timeout       *time.Duration
+	RetryInterval *time.Duration
+}
+
+// WaitForDHCPEntries waits for at least one dhcp entry with the correct mac address.
+// This function can be used to wait for an instance to use dhcp
+func (s *API) WaitForDHCPEntries(req *WaitForDHCPEntriesRequest, opts ...scw.RequestOption) (*ListDHCPEntriesResponse, error) {
+	timeout := defaultTimeout
+	if req.Timeout != nil {
+		timeout = *req.Timeout
+	}
+	retryInterval := defaultRetryInterval
+	if req.RetryInterval != nil {
+		retryInterval = *req.RetryInterval
+	}
+
+	dhcpEntries, err := async.WaitSync(&async.WaitSyncConfig{
+		Get: func() (interface{}, bool, error) {
+			entries, err := s.ListDHCPEntries(&ListDHCPEntriesRequest{
+				Zone:             req.Zone,
+				GatewayNetworkID: req.GatewayNetworkID,
+				MacAddress:       &req.MacAddress,
+			}, opts...)
+			if err != nil {
+				return nil, false, err
+			}
+
+			containsMacAddress := false
+			for _, entry := range entries.DHCPEntries {
+				if entry.MacAddress == req.MacAddress {
+					containsMacAddress = true
+					break
+				}
+			}
+
+			return entries, containsMacAddress, err
+		},
+		Timeout:          timeout,
+		IntervalStrategy: async.LinearIntervalStrategy(retryInterval),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "waiting for gateway network failed")
+	}
+
+	return dhcpEntries.(*ListDHCPEntriesResponse), nil
+}
