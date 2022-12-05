facb0d148b455be77e19841d1b97468b4f1b56b7
Yacine Fodil <105779815+yfodil@users.noreply.github.com>
2022-12-05 10:28:34 +0100

    feat(baremetal): add wait method for server private networks (#1470)

--- api/baremetal/v1/server_utils.go
+++ api/baremetal/v1/server_utils.go
@@ -207,3 +207,58 @@ func (s *API) WaitForServerOptions(req *WaitForServerOptionsRequest, opts ...scw
 
 	return server.(*Server), nil
 }
+
+// WaitForServerPrivateNetworksRequest is used by WaitForServerPrivateNetworks method.
+type WaitForServerPrivateNetworksRequest struct {
+	ServerID      string
+	Zone          scw.Zone
+	Timeout       *time.Duration
+	RetryInterval *time.Duration
+}
+
+// WaitForServerPrivateNetworks wait for all server private networks to be in a "terminal state" before returning.
+// This function can be used to wait for all server private networks to be set.
+func (s *PrivateNetworkAPI) WaitForServerPrivateNetworks(req *WaitForServerPrivateNetworksRequest, opts ...scw.RequestOption) ([]*ServerPrivateNetwork, error) {
+	timeout := defaultTimeout
+	if req.Timeout != nil {
+		timeout = *req.Timeout
+	}
+	retryInterval := defaultRetryInterval
+	if req.RetryInterval != nil {
+		retryInterval = *req.RetryInterval
+	}
+
+	terminalStatus := map[ServerPrivateNetworkStatus]struct{}{
+		ServerPrivateNetworkStatusAttached: {},
+		ServerPrivateNetworkStatusError:    {},
+		ServerPrivateNetworkStatusUnknown:  {},
+		ServerPrivateNetworkStatusLocked:   {},
+	}
+
+	serverPrivateNetwork, err := async.WaitSync(&async.WaitSyncConfig{
+		Get: func() (interface{}, bool, error) {
+			res, err := s.ListServerPrivateNetworks(&PrivateNetworkAPIListServerPrivateNetworksRequest{
+				ServerID: &req.ServerID,
+				Zone:     req.Zone,
+			}, opts...)
+			if err != nil {
+				return nil, false, err
+			}
+
+			for i := range res.ServerPrivateNetworks {
+				_, isTerminal := terminalStatus[res.ServerPrivateNetworks[i].Status]
+				if !isTerminal {
+					return res, isTerminal, nil
+				}
+			}
+			return res, true, err
+		},
+		Timeout:          timeout,
+		IntervalStrategy: async.LinearIntervalStrategy(retryInterval),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "waiting for server private networks failed")
+	}
+
+	return serverPrivateNetwork.([]*ServerPrivateNetwork), nil
+}
