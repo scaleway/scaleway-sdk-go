57472e27396380a18db9f31eab11930a52ade169
Jules Casteran <jcasteran@scaleway.com>
2022-12-14 11:54:35 +0100

    fix defaultlogger init

--- logger/default_logger.go
+++ logger/default_logger.go
@@ -22,6 +22,7 @@ type loggerT struct {
 // Not mutex-protected, should be called before any scaleway-sdk-go functions.
 func (g *loggerT) Init(w io.Writer, level LogLevel) {
 	g.m = newLogger(w, level).m
+	g.v = level
 }
 
 // Debugf logs to the DEBUG log. Arguments are handled in the manner of fmt.Printf.
