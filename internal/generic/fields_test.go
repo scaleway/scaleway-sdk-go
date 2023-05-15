a717475a6a0e394a0dd8f46574bb86e284499fdc
Jules Castéran <jcasteran@scaleway.com>
2023-05-15 11:53:49 +0200

    fix: sorting resources without locality when listing multiple localities (#1691)

---
+++ internal/generic/fields_test.go
@@ -0,0 +1,60 @@
+package generic
+
+import "testing"
+
+func TestHasField(t *testing.T) {
+	tests := []struct {
+		name      string
+		i         interface{}
+		fieldName string
+		want      bool
+	}{
+		{
+			"native type",
+			struct {
+				Zone string
+			}{},
+			"Zone",
+			true,
+		},
+		{
+			"ptr type",
+			struct {
+				Zone *string
+			}{},
+			"Zone",
+			true,
+		},
+		{
+			"invalid case",
+			struct {
+				Zone *string
+			}{},
+			"zone",
+			false,
+		},
+		{
+			"slice",
+			[]struct {
+				Zone string
+			}{},
+			"Zone",
+			true,
+		},
+		{
+			"slice invalid case",
+			[]struct {
+				Zone string
+			}{},
+			"zone",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HasField(tt.i, tt.fieldName); got != tt.want {
+				t.Errorf("HasField() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
