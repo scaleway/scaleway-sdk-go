e949fbf070372b52bf8571b19fba9e5e2e4ce3e7
Jules Casteran <jcasteran@scaleway.com>
2023-05-15 11:27:37 +0200

    fix for go1.17

--- internal/generic/ptr.go
+++ internal/generic/ptr.go
@@ -3,7 +3,7 @@ package generic
 import "reflect"
 
 func indirectType(typ reflect.Type) reflect.Type {
-	if typ.Kind() == reflect.Pointer {
+	if typ.Kind() == reflect.Ptr {
 		return typ.Elem()
 	}
 
