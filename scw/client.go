5e9b8d0fe6a3480ea525ca8f2235f6bb371433fd
Jules Castéran <jules.casteran@gmail.com>
2023-08-09 11:21:03 +0200

    Revert "feat(scw): allow usage of http proxy (#1794)"
    This reverts commit a7e6dd2091a25c9f8331e84b09d4102738ca018d.

--- scw/client.go
+++ scw/client.go
@@ -548,7 +548,6 @@ func newHTTPClient() *http.Client {
 	return &http.Client{
 		Timeout: 30 * time.Second,
 		Transport: &http.Transport{
-			Proxy:                 http.ProxyFromEnvironment,
 			DialContext:           (&net.Dialer{Timeout: 5 * time.Second}).DialContext,
 			TLSHandshakeTimeout:   5 * time.Second,
 			ResponseHeaderTimeout: 30 * time.Second,
