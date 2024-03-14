0e539b7dbd50e303e92c2dd0faa4fd37bd7c1a75
Jules Casteran <jcasteran@scaleway.com>
2024-03-14 08:31:52 +0100

    update sdk_compilation_test

--- sdk_compilation_test.go
+++ sdk_compilation_test.go
@@ -10,7 +10,6 @@ import (
 	_ "github.com/scaleway/scaleway-sdk-go/api/iot/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/k8s/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/lb/v1"
-	_ "github.com/scaleway/scaleway-sdk-go/api/marketplace/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/registry/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/test/v1"
