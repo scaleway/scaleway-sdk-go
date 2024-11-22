a2b4016183f33177f4e7cab4ac40c8585c59d5d3
Rémy Léone <rleone@scaleway.com>
2024-11-22 10:45:06 +0100

    fix

--- sdk_compilation_test.go
+++ sdk_compilation_test.go
@@ -3,7 +3,7 @@ package scalewaysdkgo
 // This test file makes sure that all the auto-generated code compiles.
 
 import (
-	_ "github.com/scaleway/scaleway-sdk-go/api/account/v2alpha1"
+	_ "github.com/scaleway/scaleway-sdk-go/api/account/v3"
 	_ "github.com/scaleway/scaleway-sdk-go/api/baremetal/v1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/domain/v2beta1"
 	_ "github.com/scaleway/scaleway-sdk-go/api/instance/v1"
