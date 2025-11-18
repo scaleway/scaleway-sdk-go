28470fd7dcfc01c16f8a484e94c1dda6bce8c7f4
Jonathan Remy <jremy@scaleway.com>
2025-11-18 09:31:35 +0100

    fix(cockpit): remove WaitForAlert and keep only WaitForPreconfiguredAlerts

--- api/cockpit/v1/cockpit_utils.go
+++ api/cockpit/v1/cockpit_utils.go
@@ -13,60 +13,6 @@ const (
 	defaultTimeout       = 5 * time.Minute
 )
 
-// WaitForAlertRequest is used by WaitForAlert method.
-type WaitForAlertRequest struct {
-	Region        scw.Region
-	AlertID       string
-	Timeout       *time.Duration
-	RetryInterval *time.Duration
-}
-
-// WaitForAlert waits for the alert to be in a "terminal state" before returning.
-// This function can be used to wait for an alert to be enabled or disabled.
-func (s *RegionalAPI) WaitForAlert(req *WaitForAlertRequest, opts ...scw.RequestOption) (*Alert, error) {
-	timeout := defaultTimeout
-	if req.Timeout != nil {
-		timeout = *req.Timeout
-	}
-	retryInterval := defaultRetryInterval
-	if req.RetryInterval != nil {
-		retryInterval = *req.RetryInterval
-	}
-
-	terminalStatus := map[AlertStatus]struct{}{
-		AlertStatusEnabled:  {},
-		AlertStatusDisabled: {},
-	}
-
-	alert, err := async.WaitSync(&async.WaitSyncConfig{
-		Get: func() (any, bool, error) {
-			// List all alerts and find the one with matching ID
-			res, err := s.ListAlerts(&RegionalAPIListAlertsRequest{
-				Region: req.Region,
-			}, opts...)
-			if err != nil {
-				return nil, false, err
-			}
-
-			// Find the alert by ID
-			for _, alert := range res.Alerts {
-				if alert.ID == req.AlertID {
-					_, isTerminal := terminalStatus[alert.RuleStatus]
-					return alert, isTerminal, nil
-				}
-			}
-
-			return nil, false, errors.New("alert not found")
-		},
-		Timeout:          timeout,
-		IntervalStrategy: async.LinearIntervalStrategy(retryInterval),
-	})
-	if err != nil {
-		return nil, errors.Wrap(err, "waiting for alert failed")
-	}
-	return alert.(*Alert), nil
-}
-
 // WaitForPreconfiguredAlertsRequest is used by WaitForPreconfiguredAlerts method.
 type WaitForPreconfiguredAlertsRequest struct {
 	Region             scw.Region
@@ -90,17 +36,14 @@ func (s *RegionalAPI) WaitForPreconfiguredAlerts(req *WaitForPreconfiguredAlerts
 		retryInterval = *req.RetryInterval
 	}
 
-	// For enabling/disabling, accept transitional states as terminal
 	var terminalStatus map[AlertStatus]struct{}
 	switch req.TargetStatus {
 	case AlertStatusEnabled:
-		// Accept both enabled and enabling as terminal states
 		terminalStatus = map[AlertStatus]struct{}{
 			AlertStatusEnabled:  {},
 			AlertStatusEnabling: {},
 		}
 	case AlertStatusDisabled:
-		// Accept both disabled and disabling as terminal states
 		terminalStatus = map[AlertStatus]struct{}{
 			AlertStatusDisabled:  {},
 			AlertStatusDisabling: {},
@@ -113,7 +56,6 @@ func (s *RegionalAPI) WaitForPreconfiguredAlerts(req *WaitForPreconfiguredAlerts
 
 	result, err := async.WaitSync(&async.WaitSyncConfig{
 		Get: func() (any, bool, error) {
-			// List all preconfigured alerts for the project
 			res, err := s.ListAlerts(&RegionalAPIListAlertsRequest{
 				Region:          req.Region,
 				ProjectID:       req.ProjectID,
@@ -123,7 +65,6 @@ func (s *RegionalAPI) WaitForPreconfiguredAlerts(req *WaitForPreconfiguredAlerts
 				return nil, false, err
 			}
 
-			// Create a map of rule ID to alert
 			alertsByRuleID := make(map[string]*Alert)
 			for _, alert := range res.Alerts {
 				if alert.PreconfiguredData != nil && alert.PreconfiguredData.PreconfiguredRuleID != "" {
@@ -131,7 +72,6 @@ func (s *RegionalAPI) WaitForPreconfiguredAlerts(req *WaitForPreconfiguredAlerts
 				}
 			}
 
-			// Check if all requested alerts are in terminal state
 			var matchedAlerts []*Alert
 			for _, ruleID := range req.PreconfiguredRules {
 				alert, found := alertsByRuleID[ruleID]
@@ -141,14 +81,12 @@ func (s *RegionalAPI) WaitForPreconfiguredAlerts(req *WaitForPreconfiguredAlerts
 
 				_, isTerminal := terminalStatus[alert.RuleStatus]
 				if !isTerminal {
-					// At least one alert is not in terminal state, continue waiting
 					return nil, false, nil
 				}
 
 				matchedAlerts = append(matchedAlerts, alert)
 			}
 
-			// All alerts are in terminal state
 			return matchedAlerts, true, nil
 		},
 		Timeout:          timeout,
@@ -159,4 +97,3 @@ func (s *RegionalAPI) WaitForPreconfiguredAlerts(req *WaitForPreconfiguredAlerts
 	}
 	return result.([]*Alert), nil
 }
-
