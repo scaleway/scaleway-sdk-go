1cea4810c436a090eb67a15277db84501921adb7
Geoffrey JOUNT <gjount@scaleway.com>
2026-08-07 11:24:52 +0200

    fix tests

--- scw/client_option_test.go
+++ scw/client_option_test.go
@@ -126,7 +126,7 @@ func TestClientOptions(t *testing.T) {
 				s.token = auth.NewToken(testAccessKey, testSecretKey)
 				s.defaultRegion = &v
 			},
-			errStr: "scaleway-sdk-go: invalid default region format 'invalid', available regions are: fr-par, nl-ams, pl-waw, it-mil",
+			errStr: "scaleway-sdk-go: invalid default region format 'invalid', available regions are: fr-par, fr-snc, nl-ams, pl-waw, it-mil",
 		},
 		{
 			name: "Should throw a zone error",
@@ -144,7 +144,7 @@ func TestClientOptions(t *testing.T) {
 				s.token = auth.NewToken(testAccessKey, testSecretKey)
 				s.defaultZone = &v
 			},
-			errStr: "scaleway-sdk-go: invalid default zone format 'invalid', available zones are: fr-par-1, fr-par-2, fr-par-3, nl-ams-1, nl-ams-2, nl-ams-3, pl-waw-1, pl-waw-2, pl-waw-3, it-mil-1",
+			errStr: "scaleway-sdk-go: invalid default zone format 'invalid', available zones are: fr-par-1, fr-par-2, fr-par-3, fr-snc-1, fr-snc-2, fr-snc-3, nl-ams-1, nl-ams-2, nl-ams-3, pl-waw-1, pl-waw-2, pl-waw-3, it-mil-1",
 		},
 	}
 
