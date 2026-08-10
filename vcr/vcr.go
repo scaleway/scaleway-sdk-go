07710732ce63f0084a55cc27539b0394da805544
Leïla Marabese <lmarabese@scaleway.com>
2026-08-10 11:41:23 +0200

    feat(vcr): remove X-Registry-Auth from headers in cassettes

--- vcr/vcr.go
+++ vcr/vcr.go
@@ -37,6 +37,7 @@ func cassetteRequestFilter(i *cassette.Interaction) error {
 	delete(i.Request.Headers, "x-auth-token")
 	delete(i.Request.Headers, "X-Auth-Token")
 	delete(i.Request.Headers, "Authorization")
+	delete(i.Request.Headers, "X-Registry-Auth")
 
 	// URL substitutions
 	i.Request.URL = regexp.MustCompile(`(.+)organization_id=[0-9a-f-]{36}(.+)`).
