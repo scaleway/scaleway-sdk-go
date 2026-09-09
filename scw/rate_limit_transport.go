4efe62414b0ad63df1bf2afff74010a7535a2e3e
Geoffrey JOUNT <gjount@scaleway.com>
2026-09-09 16:15:37 +0200

    separate rate_limit.go into two files

--- scw/rate_limit_transport.go
+++ scw/rate_limit_transport.go
@@ -5,47 +5,8 @@ import (
 	"crypto/tls"
 	"net/http"
 	"strconv"
-	"sync"
 	"time"
 )
-
-// RateLimitState stores the current limit state
-type RateLimitState struct {
-	// mu protects RateLimitState from concurrent edits by different
-	// goroutines
-	mu sync.RWMutex
-
-	// remaining is the number of requests still available for this time window
-	remaining int
-
-	// resetAt is the time to wait before the next time window
-	resetAt time.Time
-}
-
-// Update updates the limit state with the headers values
-func (s *RateLimitState) Update(remaining int, resetInSeconds int) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.remaining = remaining
-	s.resetAt = time.Now().Add(time.Duration(resetInSeconds) * time.Second)
-}
-
-// GetWaitDuration computes the time to wait before the next request
-func (s *RateLimitState) GetWaitDuration() time.Duration {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	if s.remaining > 0 {
-		return 0
-	}
-
-	now := time.Now()
-	if now.Before(s.resetAt) {
-		return s.resetAt.Sub(now)
-	}
-
-	return 0
-}
 
 // RateLimitTransport implements http.RoundTripper
 type RateLimitTransport struct {
